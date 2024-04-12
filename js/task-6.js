92ab2d32d86495b6dd4b43eb884e9208df43b7c5
YevheniiaYakymenko <152288008+YevheniiaYakymenko@users.noreply.github.com>
2024-04-12 16:10:45 +0200

    css+tasks

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,35 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector("input");
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]");
+const boxDiv = document.querySelector("#boxes");
+
+function onCreateBoxes(amount) {
+  boxDiv.innerHTML = "";
+  let size = 30;
+  for (let i = 0; i < amount; i++) {
+    const div = document.createElement("div");
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    boxDiv.append(div);
+    size += 10;
+  }
+}
+
+function onDestroyBoxes() {
+  boxDiv.innerHTML = "";
+}
+
+createButton.addEventListener("click", () => {
+  const amount = parseInt(input.value);
+  if (amount >= 1 && amount <= 100) {
+    onCreateBoxes(amount);
+    input.value = "";
+  }
+});
+
+destroyButton.addEventListener("click", onDestroyBoxes);
